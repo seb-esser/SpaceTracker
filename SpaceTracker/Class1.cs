cf1b7134172c27d7b80d984f8e0cd116c8b6cbcf
Sebastian Esser <sebastian.esser@tum.de>
2022-02-14 10:34:56 +0100

    init commit

---
+++ SpaceTracker/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Analysis;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.Exceptions;
+using Autodesk.Revit.UI;
+
+namespace SpaceTracker
+{
+    public class SpaceTrackerClass : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

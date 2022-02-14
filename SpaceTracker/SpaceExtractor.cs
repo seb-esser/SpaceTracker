85dcfbf650b10064f42e266f3b0d311cf4a1bad8
Sebastian Esser <sebastian.esser@tum.de>
2022-02-14 11:04:11 +0100

    growing

---
+++ SpaceTracker/SpaceExtractor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace SpaceTracker
+{
+    public class SpaceExtractor
+    {
+        private void extractAdjacentRooms(Room room, ref Document doc)
+        {
+            IList<IList<BoundarySegment>> boundaries
+                = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
+
+            int n = boundaries.Count;
+
+            int iBoundary = 0;
+
+            foreach (IList b in boundaries)
+            {
+                ++iBoundary;
+                var iSegment = 0;
+                foreach (BoundarySegment s in b)
+                {
+                    ++iSegment;
+                    ElementId neighbourId = s.ElementId;
+
+                    Element neighbour = doc.GetElement(neighbourId);
+
+                    Curve curve = s.GetCurve();
+                    
+                    if (neighbour is Room)
+                    {
+                        Debug.WriteLine(neighbour.Id);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ SpaceTracker/SpaceTrackerClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Analysis;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.Exceptions;
+using Autodesk.Revit.UI;
+
+namespace SpaceTracker
+{
+    public class SpaceTrackerClass : IExternalApplication
+    {
+        #region register events
+
+        /// <summary>
+        /// Catch startup and mount event handlers
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                application.ControlledApplication.DocumentCreated +=
+                    new EventHandler<DocumentCreatedEventArgs>(documentCreated);
+                application.ControlledApplication.DocumentOpened +=
+                    new EventHandler<Autodesk.Revit.DB.Events.DocumentOpenedEventArgs>(documentOpened);
+                application.ControlledApplication.DocumentChanged +=
+                    new EventHandler<DocumentChangedEventArgs>(documentChanged);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                Debug.WriteLine("Something went wrong during the registration process. ");
+
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// remove mounted events
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            application.ControlledApplication.DocumentOpened -= documentOpened;
+            application.ControlledApplication.DocumentChanged -= documentChanged;
+            application.ControlledApplication.DocumentCreated -= documentCreated;
+            return Result.Succeeded;
+        }
+
+        #endregion
+
+        #region Event handler
+
+        private void documentChanged(object sender, DocumentChangedEventArgs e)
+        {
+            var addedElementIds = e.GetAddedElementIds();
+            var deletedElementIds = e.GetDeletedElementIds();
+            var modifiedElementIds = e.GetModifiedElementIds();
+            
+            
+        }
+
+        private void documentCreated(object sender, DocumentCreatedEventArgs e)
+        {
+            // get document from event args.
+            Document doc = e.Document;
+
+            // nothing to query from the existing Revit model except the "outside" node
+
+        }
+
+        private void documentOpened(object sender, DocumentOpenedEventArgs e)
+        {
+            // get document from event args.
+            Document doc = e.Document;
+
+            // get all spaces and build graph from existing model
+        }
+
+        #endregion
+    }
+}

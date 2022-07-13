bbd95de173026669caa52e6739ec2f8af2e5ffe6
Sebastian Esser <sebastian.esser@tum.de>
2022-07-13 08:11:50 +0200

    starting to implement external commands and to prepare the code base for a Design Automation mission

---
+++ SpaceTracker/GraphGeneratorCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace SpaceTracker
+{
+    [Transaction(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class GraphGeneratorCommand: IExternalCommand
+    {
+        
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            var extractor = new SpaceExtractor(); 
+            extractor.CreateInitialGraph(doc);
+           
+            return Result.Succeeded;
+        }
+    }
+}
